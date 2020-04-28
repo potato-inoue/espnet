02f9e7218134e946e07fa8ba9a3b37dea8f1cd15
potato-inoue <k_inoue@a.cs.okayama-u.ac.jp>
2020-04-28 22:10:17 +0900

    backup asr1

---
+++ egs/libritts/asr1/run.sh
@@ -0,0 +1,336 @@
+#!/bin/bash
+
+# Copyright 2019 Nagoya University (Takenori Yoshimura)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+# general configuration
+backend=pytorch
+stage=2 #-1
+stop_stage=2 #100
+ngpu=1       # number of gpu in training
+nj=12        # numebr of parallel jobs
+dumpdir=dump # directory to dump full features
+verbose=1    # verbose option (if set > 1, get more log)
+seed=1       # random seed number
+resume=""    # the snapshot path to resume (if set empty, no effect)
+
+# feature extraction related
+fs=22050 #24000      # sampling frequency
+fmax=""       # maximum frequency
+fmin=""       # minimum frequency
+n_mels=80     # number of mel basis
+n_fft=1024    # number of fft points
+n_shift=256   # number of shift points
+win_length="" # window length
+
+# config files
+# train_config=conf/train_pytorch_transformer+spkemb.yaml #train_pytorch_tacotron2+spkemb.yaml
+# train_config=conf/tuning/train_rnn_v2.yaml 
+# train_config=conf/tuning/train_pytorch_transformer_lr5.0_ag8.v2.yaml 
+train_config=conf/tuning/train_pytorch_transformer.yaml
+# decode_config=conf/decode.yaml
+decode_config=conf/tuning/decode_pytorch_transformer.yaml
+
+# decoding related
+model=model.loss.best
+n_average=1 # if > 0, the model averaged with n_average ckpts will be used instead of model.loss.best
+griffin_lim_iters=64  # the number of iterations of Griffin-Lim
+
+# Set this to somewhere where you want to put your data, or where
+# someone else has already put it. You'll want to change this
+# if you're not on the CLSP grid.
+datadir=/abelab/DB4 #/export/a15/vpanayotov/data
+
+# base url for downloads.
+data_url=www.openslr.org/resources/60
+
+# exp tag
+tag="" # tag for managing experiments.
+
+. utils/parse_options.sh || exit 1;
+
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train_clean_460
+dev_set=dev_clean
+eval_set=test_clean_asr
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    echo "stage -1: Data Download"
+    mkdir -p ${datadir}
+    for part in dev-clean test-clean train-clean-100 train-clean-360; do
+        local/download_and_untar.sh ${datadir} ${data_url} ${part}
+    done
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    ### Task dependent. You have to make data the following preparation part by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 0: Data preparation"
+    # for part in dev-clean test-clean train-clean-100 train-clean-360; do
+    #     # use underscore-separated names in data directories.
+    #     local/data_prep.sh ${datadir}/LibriTTS/${part} data/${part//-/_}
+    # done
+
+    for x in dev_clean test_clean train_clean_100 train_clean_360; do
+        utils/copy_data_dir.sh data/${x} data/${x}_22050
+        utils/data/resample_data_dir.sh ${fs} data/${x}_22050
+    done
+fi
+
+feat_tr_dir=${dumpdir}/${train_set}_22050; mkdir -p ${feat_tr_dir}
+feat_dt_dir=${dumpdir}/${dev_set}_22050; mkdir -p ${feat_dt_dir}
+feat_ev_dir=${dumpdir}/${eval_set}_22050; mkdir -p ${feat_ev_dir}
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    ### Task dependent. You have to design training and dev name by yourself.
+    ### But you can utilize Kaldi recipes in most cases
+    echo "stage 1: Feature Generation"
+
+    fbankdir=fbank
+    for x in dev_clean test_clean train_clean_100 train_clean_360; do
+        make_fbank.sh --cmd "${train_cmd}" --nj ${nj} \
+            --fs ${fs} \
+            --fmax "${fmax}" \
+            --fmin "${fmin}" \
+            --n_fft ${n_fft} \
+            --n_shift ${n_shift} \
+            --win_length "${win_length}" \
+            --n_mels ${n_mels} \
+            data/${x}_22050 \
+            exp/make_fbank/${x}_22050 \
+            ${fbankdir}
+    done
+
+    utils/combine_data.sh data/${train_set}_org_22050 data/train_clean_100_22050 data/train_clean_360_22050
+    utils/combine_data.sh data/${dev_set}_org_22050 data/dev_clean_22050
+
+    # remove utt having more than 3000 frames
+    # remove utt having more than 400 characters
+    remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${train_set}_org_22050 data/${train_set}_22050
+    remove_longshortdata.sh --maxframes 3000 --maxchars 400 data/${dev_set}_org_22050 data/${dev_set}_22050
+
+    # compute statistics for global mean-variance normalization
+    compute-cmvn-stats scp:data/${train_set}_22050/feats.scp data/${train_set}_22050/cmvn.ark
+
+    # dump features for training
+    dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta false \
+        data/${train_set}_22050/feats.scp data/${train_set}_22050/cmvn.ark exp/dump_feats/train_22050 ${feat_tr_dir}
+    dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta false \
+        data/${dev_set}_22050/feats.scp data/${train_set}_22050/cmvn.ark exp/dump_feats/dev_22050 ${feat_dt_dir}
+    dump.sh --cmd "$train_cmd" --nj ${nj} --do_delta false \
+        data/${eval_set}_22050/feats.scp data/${train_set}_22050/cmvn.ark exp/dump_feats/eval_22050 ${feat_ev_dir}
+fi
+
+dict=data/lang_1char/${train_set}_22050_units.txt
+echo "dictionary: ${dict}"
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    ### Task dependent. You have to check non-linguistic symbols used in the corpus.
+    echo "stage 2: Dictionary and Json Data Preparation"
+    # mkdir -p data/lang_1char/
+    # echo "<unk> 1" > ${dict} # <unk> must be 1, 0 will be used for "blank" in CTC
+    # text2token.py -s 1 -n 1 data/${train_set}_22050/text | cut -f 2- -d" " | tr " " "\n" \
+    # | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' >> ${dict}
+    # wc -l ${dict}
+
+    # make json labels
+    # data2json.sh --feat ${feat_tr_dir}/feats.scp \
+    #      data/${train_set}_22050 ${dict} > ${feat_tr_dir}/data.json
+    # data2json.sh --feat ${feat_dt_dir}/feats.scp \
+    #      data/${dev_set}_22050 ${dict} > ${feat_dt_dir}/data.json
+    data2json.sh --feat ${feat_ev_dir}/feats.scp \
+         data/${eval_set}_22050 ${dict} > ${feat_ev_dir}/data.json
+fi
+
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+    echo "stage 3: x-vector extraction"
+    # Make MFCCs and compute the energy-based VAD for each dataset
+    mfccdir=mfcc
+    vaddir=mfcc
+    for name in ${train_set}_22050 ${dev_set}_22050 ${eval_set}_22050; do
+        utils/copy_data_dir.sh data/${name} data/${name}_mfcc_16k
+        utils/data/resample_data_dir.sh 16000 data/${name}_mfcc_16k
+        steps/make_mfcc.sh \
+            --write-utt2num-frames true \
+            --mfcc-config conf/mfcc.conf \
+            --nj ${nj} --cmd "$train_cmd" \
+            data/${name}_mfcc_16k exp/make_mfcc_16k ${mfccdir}
+        utils/fix_data_dir.sh data/${name}_mfcc_16k
+        sid/compute_vad_decision.sh --nj ${nj} --cmd "$train_cmd" \
+            data/${name}_mfcc_16k exp/make_vad ${vaddir}
+        utils/fix_data_dir.sh data/${name}_mfcc_16k
+    done
+
+    # Check pretrained model existence
+    nnet_dir=exp/xvector_nnet_1a
+    if [ ! -e ${nnet_dir} ]; then
+        echo "X-vector model does not exist. Download pre-trained model."
+        wget http://kaldi-asr.org/models/8/0008_sitw_v2_1a.tar.gz
+        tar xvf 0008_sitw_v2_1a.tar.gz
+        mv 0008_sitw_v2_1a/exp/xvector_nnet_1a exp
+        rm -rf 0008_sitw_v2_1a.tar.gz 0008_sitw_v2_1a
+    fi
+    # Extract x-vector
+    for name in ${train_set}_22050 ${dev_set}_22050 ${eval_set}_22050; do
+        sid/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd --mem 4G" --nj ${nj} \
+            ${nnet_dir} data/${name}_mfcc_16k \
+            ${nnet_dir}/xvectors_${name}
+    done
+    # Update json
+    for name in ${train_set}_22050 ${dev_set}_22050 ${eval_set}_22050; do
+        local/update_json.sh ${dumpdir}/${name}/data.json ${nnet_dir}/xvectors_${name}/xvector.scp
+    done
+fi
+
+if [ -z ${tag} ]; then
+    expname=${train_set}_22050_${backend}_$(basename ${train_config%.*})
+else
+    expname=${train_set}_22050_${backend}_${tag}
+fi
+expdir=exp/${expname}
+mkdir -p ${expdir}
+if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
+    # echo "stage 4: Text-to-speech model training"
+    tr_json=${feat_tr_dir}/data_wo_xvec.json
+    dt_json=${feat_dt_dir}/data_wo_xvec.json
+    # ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+    #     tts_train.py \
+    #        --backend ${backend} \
+    #        --ngpu ${ngpu} \
+    #        --outdir ${expdir}/results \
+    #        --tensorboard-dir tensorboard/${expname} \
+    #        --verbose ${verbose} \
+    #        --seed ${seed} \
+    #        --resume ${resume} \
+    #        --train-json ${tr_json} \
+    #        --valid-json ${dt_json} \
+    #        --config ${train_config}
+
+    echo "stage 4: Network Training"
+    N=0          # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
+    debugmode=1
+    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
+        asr_train.py \
+        --backend ${backend} \
+        --ngpu ${ngpu} \
+        --minibatches ${N} \
+        --outdir ${expdir}/results \
+        --tensorboard-dir tensorboard/${expname} \
+        --verbose ${verbose} \
+        --debugmode ${debugmode} \
+        --debugdir ${expdir} \
+        --resume ${resume} \
+        --train-json ${tr_json} \
+        --valid-json ${dt_json} \
+        --dict ${dict} \
+        --config ${train_config} \
+        --ctc_type builtin 
+
+fi
+
+if [ ${n_average} -gt 0 ]; then
+    recog_model=model.last${n_average}.avg.best
+fi
+outdir=${expdir}/outputs_${model}_$(basename ${decode_config%.*})
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
+    echo "stage 5: Decoding"
+    if [ ${n_average} -gt 0 ]; then
+        average_checkpoints.py --backend ${backend} \
+                               --snapshots ${expdir}/results/snapshot.ep.* \
+                               --out ${expdir}/results/${model} \
+                               --num ${n_average}
+    fi
+    pids=() # initialize pids
+    for rtask in  ${eval_set}; do # ${dev_set}; do
+    (
+        # decode_dir=decode_${rtask}_$(basename ${decode_config%.*}) #_${lmtag}
+        decode_dir=decode_libritts_${rtask}_$(basename ${decode_config%.*})
+        # if [ ${use_wordlm} = true ]; then
+        #     recog_opts="--word-rnnlm ${lmexpdir}/rnnlm.model.best"
+        # else
+        #     recog_opts="--rnnlm ${lmexpdir}/rnnlm.model.best"
+        # fi
+        feat_recog_dir=${dumpdir}/${rtask}_22050
+        # feat_recog_dir=/work/abelab/k_inoue/espnet_work_tree/libritts_22050hz/egs/libritts/tts1/dump/${rtask}
+
+        # split data
+        splitjson.py --parts ${nj} ${feat_recog_dir}/data.json
+
+        #### use CPU for decoding
+        ngpu=0
+        debugmode=1
+        # recog_model=model.loss.best
+
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
+            asr_recog.py \
+            --config ${decode_config} \
+            --ngpu ${ngpu} \
+            --backend ${backend} \
+            --debugmode ${debugmode} \
+            --verbose ${verbose} \
+            --recog-json ${feat_recog_dir}/split${nj}utt/data.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --model ${expdir}/results/${recog_model} 
+            
+            # ${recog_opts}
+
+        score_sclite.sh ${expdir}/${decode_dir} ${dict}
+
+        # [ ! -e ${outdir}/${name} ] && mkdir -p ${outdir}/${name}
+        # cp ${dumpdir}/${name}/data.json ${outdir}/${name}
+        # splitjson.py --parts ${nj} ${outdir}/${name}/data.json
+        # # decode in parallel
+        # ${train_cmd} JOB=1:${nj} ${outdir}/${name}/log/decode.JOB.log \
+        #     tts_decode.py \
+        #         --backend ${backend} \
+        #         --ngpu 0 \
+        #         --verbose ${verbose} \
+        #         --out ${outdir}/${name}/feats.JOB \
+        #         --json ${outdir}/${name}/split${nj}utt/data.JOB.json \
+        #         --model ${expdir}/results/${model} \
+        #         --config ${decode_config}
+        # # concatenate scp files
+        # for n in $(seq ${nj}); do
+        #     cat "${outdir}/${name}/feats.$n.scp" || exit 1;
+        # done > ${outdir}/${name}/feats.scp
+    ) &
+    pids+=($!) # store background pids
+    done
+    i=0; for pid in "${pids[@]}"; do wait ${pid} || ((i++)); done
+    [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
+fi
+
+# if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+#     echo "stage 6: Synthesis"
+#     pids=() # initialize pids
+#     for name in ${dev_set} ${eval_set}; do
+#     (
+#         [ ! -e ${outdir}_denorm/${name} ] && mkdir -p ${outdir}_denorm/${name}
+#         apply-cmvn --norm-vars=true --reverse=true data/${train_set}/cmvn.ark \
+#             scp:${outdir}/${name}/feats.scp \
+#             ark,scp:${outdir}_denorm/${name}/feats.ark,${outdir}_denorm/${name}/feats.scp
+#         convert_fbank.sh --nj ${nj} --cmd "${train_cmd}" \
+#             --fs ${fs} \
+#             --fmax "${fmax}" \
+#             --fmin "${fmin}" \
+#             --n_fft ${n_fft} \
+#             --n_shift ${n_shift} \
+#             --win_length "${win_length}" \
+#             --n_mels ${n_mels} \
+#             --iters ${griffin_lim_iters} \
+#             ${outdir}_denorm/${name} \
+#             ${outdir}_denorm/${name}/log \
+#             ${outdir}_denorm/${name}/wav
+#     ) &
+#     pids+=($!) # store background pids
+#     done
+#     i=0; for pid in "${pids[@]}"; do wait ${pid} || ((i++)); done
+#     [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
+#     echo "Finished."
+# fi
